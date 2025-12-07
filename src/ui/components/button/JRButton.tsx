012352af1dd383cd3f61ce950e34e3c0d060d6ee
owlprojection <bagus.yanuar613@gmail.com>
2025-12-07 22:43:55 +0700

    feat: textfield

--- src/ui/components/button/JRButton.tsx
+++ src/ui/components/button/JRButton.tsx
@@ -3,49 +3,6 @@ import styled, { keyframes } from 'styled-components'
 import { getVariantColor } from '../../../common/utils/generator'
 import type { IconType } from 'react-icons'
 
-const spin = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-
-  to {
-    transform: rotate(360deg);
-  }
-`;
-
-
-const Button = styled.button<{ $variant?: 'primary' | 'accent' }>`
-  padding: 0.5rem 1.25rem;
-  background-color: ${({ theme, $variant }) => getVariantColor($variant, theme, 'bg')};
-  color: white;
-  cursor: pointer;
-  border: none;
-  border: 1px solid ${({ theme, $variant }) => getVariantColor($variant, theme, 'bg')};
-  border-radius: 0.25rem;
-  transition: all 0.3s ease-in-out;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 0.25rem;
-
-  &:hover {
-    background-color: ${({ theme, $variant }) => getVariantColor($variant, theme, 'hover')};
-    border-color: ${({ theme, $variant }) => getVariantColor($variant, theme, 'hover')};
-  }
-
-  span {
-    color: white;
-  }
-
-  .loading-icon {
-    animation: ${spin} 1s linear infinite;
-    height: 1rem;
-    width: 1rem;
-    color: white;
-    fill: #9ca3af;
-  }
-`;
-
 interface IProps {
   className?: string
   text?: string
@@ -91,3 +48,47 @@ const JRButton: React.FC<IProps> = ({
 }
 
 export default JRButton
+
+const spin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+
+const Button = styled.button<{ $variant?: 'primary' | 'accent' }>`
+  padding: 0.5rem 1.25rem;
+  background-color: ${({ theme, $variant }) => getVariantColor($variant, theme, 'bg')};
+  color: white;
+  cursor: pointer;
+  border: none;
+  border: 1px solid ${({ theme, $variant }) => getVariantColor($variant, theme, 'bg')};
+  border-radius: 0.25rem;
+  transition: all 0.3s ease-in-out;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 0.25rem;
+  font-size: 0.875rem;
+
+  &:hover {
+    background-color: ${({ theme, $variant }) => getVariantColor($variant, theme, 'hover')};
+    border-color: ${({ theme, $variant }) => getVariantColor($variant, theme, 'hover')};
+  }
+
+  span {
+    color: white;
+  }
+
+  .loading-icon {
+    animation: ${spin} 1s linear infinite;
+    height: 0.75rem;
+    width: 0.75rem;
+    color: white;
+    fill: #9ca3af;
+  }
+`;
