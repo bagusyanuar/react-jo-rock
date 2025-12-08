7104e8cde8973ee9ea97fa7b2223417b7d4f87da
owlprojection <bagus.yanuar613@gmail.com>
2025-12-08 11:48:07 +0700

    feat: textfield prefix icon

--- src/ui/components/textfield/JRTextfield.stories.tsx
+++ src/ui/components/textfield/JRTextfield.stories.tsx
@@ -1,5 +1,6 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import JRTextfield from "./JRTextfield";
+import { LuMail } from 'react-icons/lu'
 
 const meta: Meta<typeof JRTextfield> = {
     component: JRTextfield,
@@ -10,5 +11,6 @@ export default meta;
 
 export const Primary: StoryObj<typeof JRTextfield> = {
     args: {
+        prefixIcon: LuMail
     },
 };
--- src/ui/components/textfield/JRTextfield.tsx
+++ src/ui/components/textfield/JRTextfield.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
+import type { IconType } from 'react-icons'
 
 interface IProps extends React.InputHTMLAttributes<HTMLInputElement> {
     className?: string
     inputClassName?: string
     placeholder?: string
     type?: Extract<React.HTMLInputTypeAttribute, "text" | "number">,
+    prefixIcon?: IconType
 }
 
 const JRTextfield = React.forwardRef<HTMLInputElement, IProps>((
@@ -13,11 +15,15 @@ const JRTextfield = React.forwardRef<HTMLInputElement, IProps>((
         className = '',
         inputClassName = '',
         placeholder = '',
-        type = 'text'
+        type = 'text',
+        prefixIcon: PrefixIcon
     }, ref
 ) => {
     return (
-        <TextfieldWrapper className={className}>
+        <TextfieldWrapper className={className} $prefixIcon={!!PrefixIcon}>
+            {PrefixIcon && <PrefixIconWrapper>
+                <PrefixIcon size={14} />
+            </PrefixIconWrapper>}
             <Textfield
                 ref={ref}
                 type={type}
@@ -30,12 +36,15 @@ const JRTextfield = React.forwardRef<HTMLInputElement, IProps>((
 
 export default JRTextfield
 
-const TextfieldWrapper = styled.div`
-    position: relative;
-    width: 100%;
+const TextfieldWrapper = styled.div<{ $prefixIcon?: boolean }>`
+    display: flex;
+    align-itmes: center;
+    justify-content: center;
+    width: 15rem;
     border: 1px solid #9ca3af;
     border-radius: 0.25rem;
     transition: all 0.3s ease-in-out;
+    padding-left: ${({ theme, $prefixIcon }) => $prefixIcon ? '0' : '0.5rem'};
     
 
     &:focus-within {
@@ -45,7 +54,7 @@ const TextfieldWrapper = styled.div`
 
 const Textfield = styled.input`
     width: 100%;
-    padding: 0.5rem 0.5rem;
+    padding: 0.5rem 0;
     font-size: 0.75rem;
     border-radius: 0.25rem;
     line-height: 1;
@@ -59,3 +68,11 @@ const Textfield = styled.input`
         box-shadow: none;
     }
 `
+
+const PrefixIconWrapper = styled.div`
+    height: inherit;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 2rem;
+`;
