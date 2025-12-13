44b5ee8f3e7bdda38161f1118e25921110106fae
owlprojection <bagus.yanuar613@gmail.com>
2025-12-13 21:57:41 +0700

    fixing gap

--- src/ui/components/button/button.variants.ts
+++ src/ui/components/button/button.variants.ts
@@ -1,7 +1,7 @@
 import { cva } from 'class-variance-authority'
 
 export const buttonVariants = cva(
-    'flex items-center justify-center gap-1 px-3 py-2.5 text-sm rounded-md border leading-none transition-colors duration-300 ease-in-out focus:outline-none cursor-pointer',
+    'flex items-center justify-center gap-1.5 px-3 py-2.5 text-sm rounded-md border leading-none transition-colors duration-300 ease-in-out focus:outline-none cursor-pointer',
     {
         variants: {
             variant: {
